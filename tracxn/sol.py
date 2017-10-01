48e6a5d7270306778f11824ea9e8fcdfeca31992
TORAN <toran.sahu@yahoo.com>
2017-10-01 11:32:24 +0530

    followed pep8

--- tracxn/sol.py
+++ tracxn/sol.py
@@ -1,7 +1,14 @@
 #!bin/python3
+"""Solution of question 1."""
 
-#if msg is the encrypted message string, then
 
+def validate(word):
+    """Do validates a word from msg."""
+    pass
+
+
+msg = '1234502468.123.456135'
+# if msg is the encrypted message string, then
 msg_list = msg.split('.')
 decrypted_msg = ''
 
@@ -9,14 +16,14 @@ pad_count = 0
 pad_flag = True
 
 for word in msg_list:
-    for i in range(len(word),0):
+    for i in range(len(word), 0):
         if(int(word[i-1] + 2) == int(word[i])):
             idx = i-1
-            pad_count+=1
+            pad_count += 1
         else:
             pad_flag = False
             break
-    if(pad_count>=2 and pad_flag == True):
+    if(pad_count >= 2 and pad_flag):
         decrypted_msg = decrypted_msg + word[0:idx] + '.'
     else:
         decrypted_msg = decrypted_msg + word + '.'
@@ -31,7 +38,5 @@ for word in decrypted_msg:
         valid_flag = False
         break
 
-print("The original msg was: ",msg," , and processed msg is :",decrypted_msg," whose validity is:",valid_flag)
-
-
-def validate(word):
+print("The original msg was: ", msg, ", and processed msg is :", decrypted_msg,
+      " whose validity is:", valid_flag)
